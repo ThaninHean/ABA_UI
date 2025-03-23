b9488766e09528c65ae1d1a4cc9d993d5a44cce7
thaninhean99@gmail.com <thaninhean99@gmial.com>
2025-03-24 02:53:20 +0700

    SplashScreen

---
+++ app/src/main/java/com/example/aba_bank/ui/SplashscreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.aba_bank.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.aba_bank.R;
+
+public class SplashscreenActivity extends AppCompatActivity {
+    ImageView splashscreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        splashscreen = findViewById(R.id.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashscreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+}
